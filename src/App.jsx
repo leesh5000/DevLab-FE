d8e807f4cf58442b796ee711d308fef67f3dac53
leesh5000 <leesh50000@gmail.com>
2023-09-16 05:47:44 +0900

    #3 - 홈 화면 개발
    홈 화면의 기본 뼈대를 개발하였습니다. 디자인 작업은 tailwindcss를 사용하였습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Home from "./pages/Home.jsx";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello World!
-      </h1>
+      <Home/>
     </>
   )
 }
