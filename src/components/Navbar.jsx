8698884c4ad90a2d550b7a4574db738077bf851e
leesh5000 <leesh50000@gmail.com>
2023-10-22 02:33:10 +0900

    #20 - code polishing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import {useEffect, useState} from "react";
 import Categories from "../utils/Categories.jsx";
-import {Link, useNavigate, useSearchParams} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 
 const Navbar = () => {
 
   const [searchParams] = useSearchParams();
   const [active, setActive] = useState("ALL");
-  const navigate = useNavigate();
 
   useEffect(() => {
     setActive(searchParams.get("category") || "ALL");
