82f3bb79d3eaa6e4e04a90d94e5dea3f0bcf2795
leesh5000 <leesh50000@gmail.com>
2023-11-02 20:29:22 +0900

    #50 - 로고 추가

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "../public/logo.png"
 
 export const Logo = () => {
   return (
     <a href="/" className="flex items-center">
+      <img src={logo} alt="logo" className="h-10 mr-3"/>
       <span className="self-center my-4 text-3xl font-semibold whitespace-nowrap dark:text-white">DevMoa</span>
     </a>
-  )
+  );
 }
