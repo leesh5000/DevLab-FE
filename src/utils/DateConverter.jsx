6c92b6651b8afea758975f87c30643635de9d479
leesh5000 <leesh50000@gmail.com>
2023-11-03 20:03:54 +0900

    게시글 날짜 표시 '0'도 보이도록 수정

--- src/utils/DateConverter.jsx
+++ src/utils/DateConverter.jsx
@@ -3,7 +3,7 @@ export const DateConverter = (timeInMillis) => {
   const localDate = new Date(timeInMillis);
   const year = localDate.getFullYear();
   const month = localDate.getMonth() + 1;
-  const day = localDate.getDate();
+  const day = localDate.getDate() < 10 ? "0" + localDate.getDate() : localDate.getDate();
   const hours = ("0" + localDate.getHours()).slice(-2);
   const minutes = ("0" + localDate.getMinutes()).slice(-2);
   const today = new Date().getDate();
