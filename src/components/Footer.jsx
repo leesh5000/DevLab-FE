6fc1f951b21d934e81944362bb385098001724d6
leesh5000 <leesh50000@gmail.com>
2023-11-04 23:30:01 +0900

    #59 - A 태그 링크 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
           문의 : devlab2023@gmail.com
         </span>
         <span className="text-sm text-gray-500 sm:text-center">
-          © 2023<a href="https://flowbite.com/" className="hover:underline"> DevMoa </a>All Rights Reserved.
+          © 2023<a href="/" className="hover:underline"> DevMoa </a>All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
           <li>
@@ -16,10 +16,10 @@ export const Footer = () => {
             <a href="/private" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+            <a href="/" className="mr-4 hover:underline md:mr-6">Licensing</a>
           </li>
           <li>
-            <a href="#" className="hover:underline">Contact</a>
+            <a href="/" className="hover:underline">Contact</a>
           </li>
         </ul>
       </div>
